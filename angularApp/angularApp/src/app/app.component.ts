45fe54f55e508ca5a705c0482cec81ebf145397d
abhijitgode99 <abhijitgode99@gmail.com>
2023-07-16 22:20:54 +0530

    task 14/07

--- angularApp/angularApp/src/app/app.component.ts
+++ angularApp/angularApp/src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularApp';
-
+  
   data: string = 'Angular Batch 7';
 }
--- angularApp/angularApp/src/app/child/child.component.ts
+++ angularApp/angularApp/src/app/child/child.component.ts
@@ -7,10 +7,20 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
   @Input() dataInput: string;
+
+// Assignment Databinding
+  userName:string='';
+
+  isEmpty():boolean {
+    return this.userName.length == 0;
+  }
+  reset(){
+    this.userName='';
+  }
+  
   constructor() {}
 
   ngOnInit() {
-    console.log('Input data from parent', this.dataInput)
+    console.log('Input data from parent', this.dataInput);
   }
-
 }
