946d5e1f44477a610739787e5479d186b2b60879
abhijitgode99 <abhijitgode99@gmail.com>
2023-07-27 23:47:52 +0530

    daily update

--- angularApp/angularApp/src/app/simple-form/simple-form.component.ts
+++ angularApp/angularApp/src/app/simple-form/simple-form.component.ts
@@ -8,20 +8,45 @@ import { NgForm } from '@angular/forms';
 })
 export class SimpleFormComponent implements OnInit {
 
-  
+  isFormSubmitted=false;
   username = '';
   defaultCourseValue = 'JavaScript';
+  genders = [
+    { id: '1', value: 'Male'},
+    { id: '2', value: 'Female'},
+  ]
+
+  formData = {
+    username: '',
+    email: '',
+    course: '',
+    country: '',
+    state: '',
+    gender:''
+   }
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {  
   }
 
 OnSubmit(form: NgForm) {
+  this.isFormSubmitted=true;
     console.log('form submitted', form);
     let email = form.value.email;
     console.log('Email is', email);
+
     let country = form.value.address.country;
-    console.log('user input country name is', country)
+    console.log('user input country name is', country);
+
+    this.formData.username = form.value.username;
+    this.formData.email = form.value.email;
+    this.formData.course = form.value.course;
+    this.formData.country = form.value.address.country;
+    this.formData.state = form.value.address.state;
+    this.formData.gender = form.value.gender;
+    // reser the form controls
+    form.reset();
+    this.defaultCourseValue = 'JavaScript';
   }
 }
